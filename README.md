921730f9621c7e431b2331491e66158571a1ea78
ChenHaifeng <m@cheft.cn>
2017-06-06 15:57:20 +0800

    add koa server

---
+++ README.md
@@ -0,0 +1,8 @@
+# next-starter-kit
+React &amp; Next Starter Kit
+
+```shell
+  npm install // 安装依赖
+  
+  npm run dev // 启动开发模式
+```
---
+++ server.js
@@ -0,0 +1,35 @@
+const Koa = require('koa')
+const next = require('next')
+const Router = require('koa-router')
+
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+app.prepare()
+.then(() => {
+  const server = new Koa()
+  const router = new Router()
+
+  // :TODO 自定义 router
+  router.get('/a', async ctx => {
+    await app.render(ctx.req, ctx.res, '/b', ctx.query)
+    ctx.respond = false
+  })
+
+  router.get('*', async ctx => {
+    await handle(ctx.req, ctx.res)
+    ctx.respond = false
+  })
+
+  server.use(async (ctx, next) => {
+    ctx.res.statusCode = 200
+    await next()
+  })
+
+  server.use(router.routes())
+  server.listen(3000, (err) => {
+    if (err) throw err
+    console.log('> Ready on http://localhost:3000')
+  })
+})
